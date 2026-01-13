85eccbd5bf6194f186063853316d51c8ea24ee84
HubTakenIs <hubertaslol@gmail.com>
2026-01-13 22:08:29 +0000

    system prompts

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import argparse
 import os
-
+from prompts import system_prompt
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
@@ -23,7 +23,8 @@ def main():
     prompt_text = args.prompt
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents=messages
+        contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt,temperature=0),
     )
     if response.usage_metadata is None:
         raise RuntimeError("No token meta data returned.")
